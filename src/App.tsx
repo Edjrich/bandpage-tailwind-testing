a73ba47763bb8e37899d4def74327aa2c65c0278
erich <ericdjrichardson@gmail.com>
2024-02-12 12:28:56 -0500

    changing file locatioons

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Bio</li>
+          <li>Socials</li>
+          <li>Contact</li>
+          <li>Music</li>
+          <li>Sample</li>
+        </ul>
+      </nav>
+
+      <h1>BUY MY BOOK</h1>
+    </>
+  );
+}
+
+export default App;
